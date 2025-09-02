4d7ffd4839a5d6a9e6484384fc6a29de2fe37782
Ma zhiheng <235997828@qq.com>
2025-09-02 19:54:24 +0800

    Level-0: Rename Duke to Milo, add greeting and exit message

---
+++ src/main/java/Milo.java
@@ -0,0 +1,12 @@
+public class Milo {
+    public static void main(String[] args) {
+        String logo = " __  __ _ _       \n"
+                + "|  \\/  (_) | ___  \n"
+                + "| |\\/| | | |/ _ \\ \n"
+                + "| |  | | | |  __/ \n"
+                + "|_|  |_|_|_|\\___| \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
