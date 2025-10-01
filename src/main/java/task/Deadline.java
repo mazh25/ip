5f13ecedc0a86e197a7b0f5323374e0d77655d19
Ma Zhiheng <e1671904@u.nus.edu>
2025-10-01 13:47:24 +0800

    feat: finish task in level-8

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,24 +1,30 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    private LocalDate by; // 用 LocalDate 代替 String
 
-    public Deadline(String description, String by, boolean isDone) {
+    public Deadline(String description, String byStr, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        this.isDone = isDone;
+        this.by = LocalDate.parse(byStr); // 假设用户输入 yyyy-MM-dd
     }
-    public String getBy() {
-        return this.by;
+
+    public LocalDate getBy() {
+        return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return "[D]" + super.toString() + " (by: " + by.format(formatter) + ")";
     }
 
     @Override
     public String toSaveFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + taskDescription + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + taskDescription + " | " + this.getBy();
     }
 }
--- src/main/java/task/Events.java
+++ src/main/java/task/Events.java
@@ -1,24 +1,29 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Events extends Task {
-    protected String from;
-    protected String to;
-    public Events(String description, String from, String to, boolean isDone) {
+    private LocalDate from;
+    private LocalDate to;
+
+    public Events(String description, String fromStr, String toStr, boolean isDone) {
         super(description, isDone);
-        this.from = from;
-        this.to = to;
+        this.isDone = isDone;
+        this.from = LocalDate.parse(fromStr);
+        this.to = LocalDate.parse(toStr);
     }
-    public String getFrom() {
-        return this.from;
-    }
-    public String getTo() {
-        return this.to;
-    }
+
+    public LocalDate getFrom() { return from; }
+    public LocalDate getTo() { return to; }
+
+    @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return "[E]" + super.toString() + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
     }
     @Override
     public String toSaveFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + taskDescription + " | " + from + " | " + to;
+        return "E | " + (isDone ? "1" : "0") + " | " + taskDescription + " | " + this.getFrom() + " | " + this.getTo();
     }
 }
